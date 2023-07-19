2cefbe18076f9b34c4c5d6148bb67d3989164dc2
Luca Membrini <130695419+MembriniLuca29@users.noreply.github.com>
2023-07-19 11:26:56 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import headerComponent from './components/headerComponent.vue';
+import mainComponent from './components/mainComponent.vue';
+import footerComponent from './components/footerComponent.vue';
+
+export default {
+  name: "app",
+  components: {
+    headerComponent,
+    mainComponent,
+    footerComponent,
+  },
+};
+</script>
+
+<template>
+  <headerComponent/>
+
+  <mainComponent/>
+
+  <footerComponent/>
+
+</template>
+
+<style lang="scss" scoped>
+$brand_primary: green;
+
+h1{
+  color: $brand_primary;
+}
+</style>
